36123a48fbb2ddb67ed5ae2a4d668daa000536f9
Stephen Kang <ki6080@gmail.com>
2021-04-17 23:16:11 +0900

    매크로 헤더 분리

--- src/cppmarkup/object.hpp
+++ src/cppmarkup/object.hpp
@@ -150,7 +150,9 @@ struct object_inst_attr_init {
 } // namespace cppmarkup::impl
 
 namespace cppmarkup::marshal {
-
+/**
+ * TODO: 템플릿 함수, integral은 int64_t 읽어오는 함수로, 부동 소수점은 double로, 문자열은 
+ */
 bool parse(int32_t& d, pugi::xml_node const& s) { return true; }
 bool parse(int64_t& d, pugi::xml_node const& s) { return true; }
 bool parse(int16_t& d, pugi::xml_node const& s) { return true; }
@@ -161,87 +163,3 @@ bool parse(std::u8string& d, pugi::xml_node const& s) { return true; }
 bool parse(cppmarkup::impl::object_base& d, pugi::xml_node const& s) { return true; }
 
 } // namespace cppmarkup::marshal
-
-#define CPPMARKUP_OBJECT_TEMPLATE(template_type_name)                                \
-    struct INTERNAL_EZ_SUPER_##template_type_name                                    \
-        : public ::cppmarkup::impl::traits<INTERNAL_EZ_SUPER_##template_type_name> { \
-        using INTERNAL_EZ_super = INTERNAL_EZ_SUPER_##template_type_name;            \
-    };                                                                               \
-    struct template_type_name : public INTERNAL_EZ_SUPER_##template_type_name
-
-#ifndef CPPMARKUP_ALIGN
-#define CPPMARKUP_ALIGN 8
-#endif
-
-#define CPPMARKUP_ADD(varname, tag, default_value, ...)                                                    \
-    struct INTERNAL_EZ_INSTANCE_##varname {                                                                \
-        using value_type = decltype(default_value);                                                        \
-                                                                                                           \
-    private:                                                                                               \
-        alignas(CPPMARKUP_ALIGN) value_type _value                             = default_value;            \
-        static inline std::atomic<::cppmarkup::impl::node_property*> _node_ref = nullptr;                  \
-                                                                                                           \
-    public:                                                                                                \
-        static bool parse(void* r, size_t size, pugi::xml_node const& s)                                   \
-        {                                                                                                  \
-            assert(sizeof(value_type) == size);                                                            \
-            return ::cppmarkup::marshal::parse(*(value_type*)r, s);                                        \
-        }                                                                                                  \
-                                                                                                           \
-        static inline ::cppmarkup::impl::object_inst_init<                                                 \
-            INTERNAL_EZ_INSTANCE_##varname, value_type, CPPMARKUP_ALIGN>                                   \
-            _init{                                                                                         \
-                _node_ref,                                                                                 \
-                INTERNAL_EZ_node_list,                                                                     \
-                tag,                                                                                       \
-                sizeof _value,                                                                             \
-                &INTERNAL_EZ_description_str,                                                              \
-                &parse};                                                                                   \
-                                                                                                           \
-        operator value_type() const                                                                        \
-        {                                                                                                  \
-            return _value;                                                                                 \
-        }                                                                                                  \
-                                                                                                           \
-        INTERNAL_EZ_INSTANCE_##varname(void* owner_base)                                                   \
-        { /* Accurate offset is recalculated here. */                                                      \
-            if (auto ptr = _node_ref.exchange(nullptr))                                                    \
-            {                                                                                              \
-                printf("ptr: %p ", ptr);                                                                   \
-                ptr->offset      = (intptr_t)this - (intptr_t)owner_base;                                  \
-                ptr->next_offset = ptr->offset + sizeof *this;                                             \
-                printf("offset: %llu ~ size: %llu\n", ptr->offset, ptr->total_size);                       \
-            }                                                                                              \
-        }                                                                                                  \
-                                                                                                           \
-        INTERNAL_EZ_INSTANCE_##varname(const INTERNAL_EZ_INSTANCE_##varname& r) = default;                 \
-        INTERNAL_EZ_INSTANCE_##varname(INTERNAL_EZ_INSTANCE_##varname&& r)      = default;                 \
-        INTERNAL_EZ_INSTANCE_##varname(value_type const& r = value_type{}) : _value(r) {}                  \
-        INTERNAL_EZ_INSTANCE_##varname(value_type&& r) : _value(std::move(r)) {}                           \
-        INTERNAL_EZ_INSTANCE_##varname& operator=(const INTERNAL_EZ_INSTANCE_##varname& r) = default;      \
-        INTERNAL_EZ_INSTANCE_##varname& operator=(INTERNAL_EZ_INSTANCE_##varname&& r) = default;           \
-                                                                                                           \
-        INTERNAL_EZ_INSTANCE_##varname& operator=(value_type const& r) { return _value = r, *this; }       \
-        INTERNAL_EZ_INSTANCE_##varname& operator=(value_type&& r) { return _value = std::move(r), *this; } \
-        auto& operator()() { return _value; }                                                              \
-        auto& operator()() const { return _value; }                                                        \
-                                                                                                           \
-        __VA_ARGS__                                                                                        \
-    } varname{this};
-
-#define CPPMARKUP_ATTR(attr_name, default_value)                          \
-    alignas(CPPMARKUP_ALIGN) std::u8string attr_name;                     \
-    struct INTERNAL_EZ_ATTR_##attr_name {                                 \
-        static inline ::cppmarkup::impl::object_inst_attr_init<           \
-            INTERNAL_EZ_ATTR_##attr_name, CPPMARKUP_ALIGN>                \
-            _init{INTERNAL_EZ_node_list, u8## #attr_name, default_value}; \
-    };
-
-#define CPPMARKUP_ADD_ARRAY(template_type_name)
-#define CPPMARKUP_DESCRIPTION_BELOW(description) \
-    static inline INTERNAL_EZ_description_replacer DESCRIPTION_##__LINE__{description};
-
-#define CPPMARKUP_NESTED_OBJECT(varname, tag, ...)               \
-    CPPMARKUP_OBJECT_TEMPLATE(INTERNAL_EZ_##varname##_TEMPLATE){ \
-        __VA_ARGS__};                                            \
-    CPPMARKUP_ADD(varname, tag, INTERNAL_EZ_##varname##_TEMPLATE{})
--- tests/automation/test-object.cpp
+++ tests/automation/test-object.cpp
@@ -1,53 +1,19 @@
 #include "catch.hpp"
-#include "cppmarkup/object.hpp"
+#include "cppmarkup/cppmarkup.hpp"
+
 
 namespace test::cppmarkup::objects {
 CPPMARKUP_OBJECT_TEMPLATE(some_type)
 {
-    /*
-    struct INTERNAL_EZ_INSTANCE_varname {
-        using value_type                                 = decltype(TEMP_DEFAULTVAL);
-        alignas(CPPMARKUP_ALIGN) value_type _value = TEMP_DEFAULTVAL;
-
-        static inline cppmarkup::impl::object_inst_init<INTERNAL_EZ_INSTANCE_varname, value_type, CPPMARKUP_ALIGN> _init{
-            INTERNAL_EZ_node_list, u8"TagName", sizeof _value, &INTERNAL_EZ_description_str,
-            [](void *r, size_t size, pugi::xml_node const &s) -> bool {
-                assert(sizeof(value_type) == size);
-                return cppmarkup::marshal::parse(*(value_type *)r, s);
-            }};
-
-        operator value_type() const
-        {
-            return _value;
-        }
-
-        INTERNAL_EZ_INSTANCE_varname(const INTERNAL_EZ_INSTANCE_varname &r) = default;
-        INTERNAL_EZ_INSTANCE_varname(INTERNAL_EZ_INSTANCE_varname &&r)      = default;
-        INTERNAL_EZ_INSTANCE_varname(value_type const &r = value_type{}) : _value(r) {}
-        INTERNAL_EZ_INSTANCE_varname(value_type &&r) : _value(std::move(r)) {}
-        INTERNAL_EZ_INSTANCE_varname &operator=(const INTERNAL_EZ_INSTANCE_varname &r) = default;
-        INTERNAL_EZ_INSTANCE_varname &operator=(INTERNAL_EZ_INSTANCE_varname &&r) = default;
-
-        INTERNAL_EZ_INSTANCE_varname &operator=(value_type const &r) { return _value = r, *this; }
-        INTERNAL_EZ_INSTANCE_varname &operator=(value_type &&r) { return _value = std::move(r), *this; }
-
-        // attribute
-        std::u8string Attr1;
-        struct INTERNAL_EZ_ATTR_Attr1 {
-            static inline cppmarkup::impl::object_inst_attr_init<INTERNAL_EZ_ATTR_Attr1, CPPMARKUP_ALIGN>
-                _init{
-                    INTERNAL_EZ_node_list, u8"Attr1", u8"Attr1Default"};
-        };
-    } d;
-    */
-
-    // CPPMARKUP_DESCRIPTION_BELOW(u8"");
+    CPPMARKUP_DESCRIPTION_BELOW(u8"");
     CPPMARKUP_ADD(fooa, u8"Fooa", 3.141, CPPMARKUP_ATTR(Attr1, u8"Hell, world!"));
     CPPMARKUP_ADD(grr, u8"EOFO", 2);
 
+    CPPMARKUP_ADD2(AdminPw, 0x864321, CPPMARKUP_ATTR(Attr1, u8""); CPPMARKUP_ATTR(Attr2, u8""));
+
     CPPMARKUP_OBJECT_TEMPLATE(nested_type)
     {
-        // CPPMARKUP_DESCRIPTION_BELOW(u8"");
+        CPPMARKUP_DESCRIPTION_BELOW(u8"");
         CPPMARKUP_ADD(fooale, u8"Fooa", 3.141, CPPMARKUP_ATTR(Attr1, u8"Hell, world!"));
     };
 
@@ -62,5 +28,6 @@ TEST_CASE("instanciation")
 {
     some_type r;
     std::atomic<void*> k;
+    r.AdminPw.Attr2 = u8"df";
 }
 } // namespace test::cppmarkup::objects

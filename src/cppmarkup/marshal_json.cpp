00e3284e16c529175fabe1bdfe773ff5d41a27e0
Stephen Kang <ki6080@gmail.com>
2021-04-29 22:31:05 +0900

    덤프 함수 분리, 오브젝트 EOF NL 추가

--- src/cppmarkup/marshal_json.cpp
+++ src/cppmarkup/marshal_json.cpp
@@ -12,7 +12,6 @@ namespace {
 template <typename Ty_> void to_json_value(json_dump& to, Ty_ const& v);
 template <> void to_json_value(json_dump& to, bool const& v) { to.dest << (v ? u8"true" : u8"false"); }
 template <> void to_json_value(json_dump& to, kangsw::markup::u8string const& v) { to.dest << u8'"' << v << u8'"'; }
-template <> void to_json_value(json_dump& to, kangsw::markup::object const& v) { dump(to, v); }
 
 enum class indent_dir {
     apply,
@@ -33,6 +32,50 @@ void break_indent(json_dump& to, indent_dir dir)
         case indent_dir::conf_bwd: to.initial_indent -= to.indent; break;
         default: throw;
     }
+}
+
+#define DO_GENERATE(ty)                                      \
+    if (prop.type.is_array())                                \
+        to_json_value(to, *(std::vector<ty>*)memory);        \
+    else if (prop.type.is_map())                             \
+        to_json_value(to, *(std::map<u8string, ty>*)memory); \
+    else                                                     \
+        to_json_value(to, *(ty*)memory);                     \
+    break;
+
+void to_json_value_from_object(json_dump& to, void const* m, property const& prop);
+
+template <> void to_json_value(json_dump& to, kangsw::markup::object const& from)
+{
+    auto& props = from.props();
+    auto& out   = to.dest;
+    out << u8'{';
+    break_indent(to, indent_dir::conf_fwd);
+
+    for (int i = 0; i < props.size(); ++i) {
+        auto& prop         = props[i];
+        void const* memory = (char*)&from + prop.memory.elem_offset + prop.memory.value_offset;
+
+        break_indent(to, indent_dir::apply);
+        out << u8'"' << prop.tag << u8"\": ";
+
+        // 오브젝트 타입별로 다른 동작
+        switch (prop.type.value_type()) {
+            case element_type::null: out << u8"null"; break;
+            case element_type::boolean: DO_GENERATE(bool);
+            case element_type::integer: DO_GENERATE(int64_t);
+            case element_type::floating_point: DO_GENERATE(double);
+            case element_type::string: DO_GENERATE(u8string);
+            case element_type::object: to_json_value_from_object(to, memory, prop); break;
+            default:;
+        }
+
+        if (i + 1 < props.size()) { out << u8','; }
+    }
+
+    break_indent(to, indent_dir::conf_bwd);
+    break_indent(to, indent_dir::apply);
+    out << u8'}';
 }
 
 template <typename Ty_> void to_json_value(json_dump& to, Ty_ const& v)
@@ -87,49 +130,13 @@ void to_json_value_from_object(json_dump& to, void const* m, property const& pro
         to_json_value(to, *(object const*)m);
     }
 }
-} // namespace
 
-#define DO_GENERATE(ty)                                      \
-    if (prop.type.is_array())                                \
-        to_json_value(to, *(std::vector<ty>*)memory);        \
-    else if (prop.type.is_map())                             \
-        to_json_value(to, *(std::map<u8string, ty>*)memory); \
-    else                                                     \
-        to_json_value(to, *(ty*)memory);                     \
-    break;
+} // namespace
 
 kangsw::markup::marshalerr_t
 kangsw::markup::dump(json_dump& to, object const& from)
 {
-    auto& props = from.props();
-    auto& out   = to.dest;
-    out << u8'{';
-    break_indent(to, indent_dir::conf_fwd);
-
-    for (int i = 0; i < props.size(); ++i) {
-        auto& prop         = props[i];
-        void const* memory = (char*)&from + prop.memory.elem_offset + prop.memory.value_offset;
-
-        break_indent(to, indent_dir::apply);
-        out << u8'"' << prop.tag << u8"\": ";
-
-        // 오브젝트 타입별로 다른 동작
-        switch (prop.type.value_type()) {
-            case element_type::null: out << u8"null"; break;
-            case element_type::boolean: DO_GENERATE(bool);
-            case element_type::integer: DO_GENERATE(int64_t);
-            case element_type::floating_point: DO_GENERATE(double);
-            case element_type::string: DO_GENERATE(u8string);
-            case element_type::object: to_json_value_from_object(to, memory, prop); break;
-            default:;
-        }
-
-        if (i + 1 < props.size()) { out << u8','; }
-    }
-
-    break_indent(to, indent_dir::conf_bwd);
-    break_indent(to, indent_dir::apply);
-    out << u8'}';
+    to_json_value(to, from);
     break_indent(to, indent_dir::apply);
     return marshalerr_t::ok;
 }
@@ -139,6 +146,6 @@ kangsw::markup::dump(json_dump& to, object const& from)
 kangsw::markup::marshalerr_t
 kangsw::markup::parse(object& to, json_parse const& from)
 {
-
+    // TODO: 파서 기능 구현하기
     return marshalerr_t::fail;
 }

6c6b486b108e94fa92ce94c4fb5b491bfa97ea91
Stephen Kang <ki6080@gmail.com>
2021-05-05 23:20:40 +0900

    Extract sample markup object as different header
    This will be widely used with various markup interfaces (BSON, XML, etc...)

--- tests/automation/test-marshal-json.cpp
+++ tests/automation/test-marshal-json.cpp
@@ -1,21 +1,6 @@
 #include "catch.hpp"
-#include <cppmarkup.hpp>
-
-CPPMARKUP_OBJECT_TEMPLATE(sample_type)
-{
-    CPPMARKUP_ELEMENT(SomeBoolValue, true);
-    CPPMARKUP_ELEMENT(SomeNullValueWithAttr, nullptr,
-                      CPPMARKUP_ATTRIBUTE(SomeAttr1, "Abcd");
-                      CPPMARKUP_ATTRIBUTE(SomeAttr2, false);
-                      CPPMARKUP_ATTRIBUTE(SomeAttr3, 315.241););
-
-    CPPMARKUP_EMBED_OBJECT_begin(SomeEmbeddedObject,
-                                 CPPMARKUP_ATTRIBUTE(SomeDateValue, 1534))
-    {
-        CPPMARKUP_ELEMENT(EmbeddedArray, CPPMARKUP_ARRAY(1, 2, 3, 4, 5));
-    }
-    CPPMARKUP_EMBED_OBJECT_end(SomeEmbeddedObject)
-};
+#include "cppmarkup.hpp"
+#include "test-common-type.hpp"
 
 SCENARIO("Markup object can be marshaled from/to JSON", "[json]")
 {

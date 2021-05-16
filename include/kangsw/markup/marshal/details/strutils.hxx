d0ffedc9182a00f8854dd94eb20e75a009a4520f
Stephen Kang <ki6080@gmail.com>
2021-05-16 14:04:26 +0900

    Extract common logics from json.hxx files

--- include/kangsw/markup/marshal/details/strutils.hxx
+++ include/kangsw/markup/marshal/details/strutils.hxx
@@ -2,4 +2,60 @@
 
 namespace kangsw::refl::marshal::utils {
 
+template <size_t N_>
+static bool one_of(char const (&s)[N_], char ch) {
+    return std::end(s) != std::find(std::begin(s), std::end(s), ch);
+}
+
+static bool spacechars(char const ch) { return one_of(" \t\n\r\f\b", ch); }
+static bool between(char const ch, char a, char b) { return a <= ch && ch <= b; }
+static bool digit(char const ch) { return between(ch, '0', '9'); }
+static bool hex(char const ch) { return between(ch, '0', '9') || between(ch, 'a', 'f'); }
+static bool alphabet(char const ch) { return between(ch, 'a', 'z') | between(ch, 'A', 'Z'); }
+static bool alphanumeric(char const ch) { return digit(ch) | alphabet(ch) | (ch == '_'); }
+
+template <typename Matches_, typename Allows_ = bool (*)(char)>
+std::pair<bool, size_t> find_until_match(
+  u8str_view in, Matches_&& match, Allows_&& allow = [](char) { return true; }) {
+    for (size_t i = 0, end = in.size(); i < end; ++i) {
+        char const ch = in[i];
+
+        if (match(ch)) {
+            return {true, i};
+        } else if (!allow(ch)) {
+            break;
+        }
+    }
+    return {false, in.size()};
+}
+
+constexpr auto matcher(char const ch) {
+    return [ch](char in) { return in == ch; };
+}
+
+constexpr auto matcher(u8str_view chset) {
+    return [chset](char in) { return chset.find(in) != chset.npos; };
+}
+
+constexpr auto escape_handling_matcher(char const to_match) {
+    struct _escaped_matcher {
+        bool operator()(char const ch) const {
+            if (escaping) { // escape whatever next char is.
+                escaping = false;
+                return false;
+            } else if (ch == '\\') {
+                escaping = true;
+                return false;
+            } else if (ch == match_target) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        char const match_target;
+        mutable bool escaping = false;
+    };
+
+    return _escaped_matcher{to_match};
+}
 }
--- include/kangsw/markup/marshal/json.hxx
+++ include/kangsw/markup/marshal/json.hxx
@@ -51,11 +51,6 @@ private:
         };
     };
 
-    template <size_t N_>
-    static bool one_of(char const (&s)[N_], char ch) {
-        return std::end(s) != std::find(std::begin(s), std::end(s), ch);
-    }
-
 public:
     json_object_from_stream(u8str& out) : _out(out) {
         _st.reserve(32);
@@ -66,13 +61,6 @@ public:
     result_t operator()(char ch);
 
 private:
-    static bool spacechars(char const ch) { return one_of(" \t\n\r\f\b", ch); }
-    static bool between(char const ch, char a, char b) { return a <= ch && ch <= b; }
-    static bool digit(char const ch) { return between(ch, '0', '9'); }
-    static bool hex(char const ch) { return between(ch, '0', '9') || between(ch, 'a', 'f'); }
-    static bool alphabet(char const ch) { return between(ch, 'a', 'z') | between(ch, 'A', 'Z'); }
-    static bool alphanumeric(char const ch) { return digit(ch) | alphabet(ch) | (ch == '_'); }
-
     void pop() { _st.pop_back(); }
     void push(_nextc::type c) { _st.push_back(c); }
     void replace(_nextc::type c) { pop(), push(c); }
@@ -86,8 +74,27 @@ private:
 /** Parse string as the schema of given object. */
 class json_parse {
 public:
-    /** Parse input string_view as */
-    bool operator()(object& dest, u8str_view i);
+    /** Type of error */
+    enum class error_type {
+        ok,
+        invalid_syntax,
+        schema_unmatched,
+    };
+
+    /** Information of errors or warnings */
+    struct error_summary {
+        size_t index;
+        error_type type;
+    };
+
+    /** Parse input json string into object. Returns nothing when succeeded. */
+    std::optional<error_summary> operator()(object& dest, u8str_view i) const;
+
+private:
+    struct _visitor {
+        template <typename Ty_> error_type operator()(property_proxy<Ty_, false> p);
+        u8str_view& i;
+    };
 };
 
 /** Dump given object as json */
@@ -104,7 +111,7 @@ public:
 // //////////////////////////////////// IMPLEMENTATION ////////////////////////////////////  //
 // //////////////////////////////////// IMPLEMENTATION ////////////////////////////////////  //
 
-namespace json {
+namespace Impl {
 template <typename Ty_>
 void _dump(Ty_ const& v, string_output& o) {
     auto constexpr T = etype::from_type<Ty_>();
@@ -225,14 +232,15 @@ template <typename Ty_>
 void json_dump::_visitor::operator()(property_proxy<Ty_, true> p) {
     auto constexpr T = etype::from_type<Ty_>();
     if constexpr (!T.is_container()) {
-        json::_dump(*p, o);
+        Impl::_dump(*p, o);
     } else {
-        json::_dump(p, o);
+        Impl::_dump(p, o);
     }
 }
 
 inline json_object_from_stream::result_t json_object_from_stream::operator()(char ch) {
     if (_st.empty()) { return error; }
+    using namespace utils;
 
     switch (auto state = static_cast<std::underlying_type_t<_nextc::type>>(_st.back())) {
             // TODO: 内膏飘 贸府 眠啊 ... 葛电 冀记俊 '/' 贸府 风凭
@@ -428,8 +436,95 @@ inline json_object_from_stream::result_t json_object_from_stream::operator()(cha
     return ready;
 }
 
+namespace Impl {
+template <typename Ty_>
+json_parse::error_type _parse(Ty_& dest, u8str_view& in) {
+    return json_parse::error_type::ok;
+}
+
+
+template <>
+inline json_parse::error_type _parse(object& dest, u8str_view& in) {
+    using namespace utils;
+    if (auto [syntax_valid, position] =
+          find_until_match(in, matcher('{'), spacechars);
+        syntax_valid) {
+        in = in.substr(position + 1);
+    } else {
+        return json_parse::error_type::invalid_syntax;
+    }
+
+    for (;;) {
+        // find tag-begin
+        if (auto [quote_found, position] =
+              find_until_match(in, matcher('"'), spacechars);
+            !quote_found) {
+            if (position >= in.size()) {
+                return json_parse::error_type::invalid_syntax;
+            }
+            if (in[position] == '}') {
+                // object match done.
+                in = in.substr(position + 1);
+                return json_parse::error_type::ok;
+            }
+        } else {
+            in = in.substr(position + 1);
+        }
+
+        // find tag-end
+        auto [tag_end_quote_found, quote_pos] =
+          find_until_match(in, escape_handling_matcher('"'));
+
+        if (!tag_end_quote_found) {
+            return json_parse::error_type::invalid_syntax;
+        }
+
+        // if tag suffixed with '~@@ATTR@@', remove it and find tag.
+        auto tag_raw = in.substr(0, quote_pos);
+        
+
+        // find property by tag name.
+    }
+
+    return json_parse::error_type::ok;
+}
+
+template <typename Ty_>
+json_parse::error_type _parse(property_proxy<std::vector<Ty_>, false>& dest, u8str_view& in) {
+    return json_parse::error_type::ok;
+}
+
+template <typename Ty_>
+json_parse::error_type _parse(property_proxy<u8str_map<Ty_>, false>& dest, u8str_view& in) {
+    return json_parse::error_type::ok;
+}
+} // namespace json
+
+template <typename Ty_>
+json_parse::error_type json_parse::_visitor::operator()(property_proxy<Ty_, false> p) {
+    if constexpr (p.type().is_container()) {
+        return Impl::_parse(p, this->i);
+    } else {
+        return Impl::_parse(*p, this->i);
+    }
+}
+
+inline std::optional<json_parse::error_summary> json_parse::operator()(object& dest, u8str_view i) const {
+    auto const init = i.data();
+    if (error_type result = Impl::_parse(dest, i);
+        result != error_type::ok) //
+    {
+        error_summary error;
+        error.type  = result;
+        error.index = i.data() - init;
+        return error;
+    } else {
+        return {};
+    }
+}
+
 inline void json_dump::operator()(object const& obj, string_output o) {
-    json::_dump(obj, o);
+    Impl::_dump(obj, o);
     o << break_indent;
 }
 } // namespace kangsw::refl::marshal
